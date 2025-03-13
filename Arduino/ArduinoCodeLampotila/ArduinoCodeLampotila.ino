10f2fda3b952dcf279f857c48f1f0b5b2f1cd9bc
SanteriSuominen <santeri.suominen@kolumbus.fi>
2025-03-13 14:28:24 +0200

    updated Script temp

--- Arduino/ArduinoCodeLampotila/ArduinoCodeLampotila.ino
+++ Arduino/ArduinoCodeLampotila/ArduinoCodeLampotila.ino
@@ -1,52 +1,77 @@
-  
-  const int analogPin = A1;
-  float rawValue = 0;
-  float voltageValue = 0;
-  float temperatureValue = 0;
+
+#include <LiquidCrystal.h> 
+
+const int rs = 12, en = 11, d4 = 5, d5 = 3, d6 = 4, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int analogPin = A1;
+float voltage = 0;
+float rawValue = 0;
+float temperature = 0;
+
+unsigned long duration = 10000;
 
 void setup()
-{
+{ 
   Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  for(int i = 0; i < 3; i++)
+  {
+    lcd.write(i);
+  }
+  
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("Miettii...");
+  delay(3000);
 }
 
 void loop()
 {
+  unsigned long startTime = millis();
   rawValue = 0;
-  for(int i = 0; i <= 10; i++)
+
+  while(millis() - startTime < duration)
   {
     int temp = analogRead(analogPin);
-    if(rawValue < temp)
+    
+    if(rawValue < temp )
     {
       rawValue = temp;
-      Serial.println("New High Score!"+ String(rawValue));
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(rawValue);
+      lcd.print("   ");
+      lcd.print(rawValue * (5.0/1024.0));
+      lcd.print("v"));
+      
     }
-      Serial.println(temp);
-      delay(1000);
+    
   }
-  
-  Serial.println("=========================================");
 
-  Serial.println(rawValue);
   //Jännite arvo
-  voltageValue = convertInputToVoltage(rawValue);
-  Serial.println(String(voltageValue) + "V");
+  voltage = rawValue * (5.0/1024.0);
 
   // lämpötila arvo
-  temperatureValue = convertVoltageToTemperature(voltageValue);
-  
-  // Kaikki hyvin
-  if(temperatureValue != 999)
-  {
-    Serial.println("Temperatue " + String(temperatureValue) + "C");
-  }
-
-  Serial.println("=========================================");
+  temperature = convertVoltageToTemperature(voltage);
+  printValue(temperature);
+  delay(2000);
 }
 
-// Muunna raaka arvo jännitteekis
-float convertInputToVoltage(int input)
+void printValue(float value)
 {
-  return input * (5.0/1024.0); 
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+
+  lcd.setCursor(0, 1);
+  lcd.print(value);
+  lcd.print(" C");
+
+  return;
 }
 
 // Muunna jännite taulukon lämpötila arvoon
@@ -64,7 +89,7 @@ float convertVoltageToTemperature(float voltage)
 
   else if(voltage < 3.6)
   {
-    return 0 + (voltage - 2.5) * (10.0/1.1);
+    return (voltage - 2.5) * (10.0/1.1);
   }
 
   else if(voltage < 4.5)
@@ -81,4 +106,44 @@ float convertVoltageToTemperature(float voltage)
   {
     return 25; //max value
   }
+}
+
+void initSpecialChar()
+{
+  byte charA[8] = {
+  B01110,
+  B10001,
+  B10001,
+  B10101,
+  B10101,
+  B10001,
+  B10001,
+  B01110
+};
+
+byte charO[8] = {
+  B01110,
+  B10001,
+  B10001,
+  B10101,
+  B10101,
+  B10001,
+  B10001,
+  B01110
+};
+
+byte charR[8] = {
+  B01110,
+  B10001,
+  B10001,
+  B10111,
+  B10101,
+  B10001,
+  B10001,
+  B01110
+};
+  // Create custom characters on the LCD
+  lcd.createChar(0, charA);  // 'Ä'
+  lcd.createChar(1, charO);  // 'Ö'
+  lcd.createChar(2, charR);  // 'Å'
 }
